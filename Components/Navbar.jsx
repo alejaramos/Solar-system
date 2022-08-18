1b910cf73cfee6ed12b15c1f26ffa367fd7987e7
alejaramos <iaramosr@gmail.com>
2022-08-18 18:11:59 -0400

    first mobile version

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,27 +1,22 @@
 import { ReactNode } from "react";
 import {
   Box,
+  Heading,
   Flex,
   Avatar,
   HStack,
   Link,
-  IconButton,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  useDisclosure,
+  useMediaQuery,
   useColorModeValue,
   Stack,
   Center,
   Text,
   Grid,
   GridItem,
+  Icon,
 } from "@chakra-ui/react";
 import css from "../styles/Home.module.css";
-import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import NavbarMobile from "../Components/NavbarMobile";
 
 const Links = ["About me", "Projects", "Contact"];
 
@@ -42,47 +37,49 @@ const NavLink = ({ children }) => (
 );
 
 export default function Navbar() {
+  const [isMobile] = useMediaQuery("(max-width: 775px)");
   return (
     <Box className={css.navbar} w="100%" h="80px">
-      <Grid
-      
-        templateColumns="repeat(5, 1fr)"
-      >
-        <GridItem colSpan={2} h="10" >
-          <Box
-            display={"flex"}
-            justifyContent="center"
-            alignItems={"center"}
-            h="80px"
-            color="white"
-          >
-            <Text className={css.titleNavbar} fontSize="250%">
-              My portfolio
-            </Text>
-          </Box>
-        </GridItem>
-        <GridItem colStart={4} colEnd={6} h="10" >
-          <Box
-            fontSize="4xl"
-            display={"flex"}
-            h="80px"
-            paddingTop="30px"
-            justifyContent="center"
-          >
-            <HStack spacing={12} alignItems={"center"}>
-              <HStack
-                as={"nav"}
-                spacing={50}
-                display={{ base: "none", md: "flex" }}
-              >
-                {Links.map((link) => (
-                  <NavLink key={link}>{link}</NavLink>
-                ))}
+      {isMobile ? (
+        <NavbarMobile />
+      ) : (
+        <Grid templateColumns="repeat(5, 1fr)">
+          <GridItem colSpan={2} h="10">
+            <Box
+              display={"flex"}
+              justifyContent="center"
+              alignItems={"center"}
+              h="80px"
+              color="white"
+            >
+              <Text className={css.titleNavbar} fontSize="250%">
+                My portfolio
+              </Text>
+            </Box>
+          </GridItem>
+          <GridItem colStart={4} colEnd={6} h="10">
+            <Box
+              fontSize="4xl"
+              display={"flex"}
+              h="80px"
+              paddingTop="30px"
+              justifyContent="center"
+            >
+              <HStack spacing={12} alignItems={"center"}>
+                <HStack
+                  as={"nav"}
+                  spacing={50}
+                  display={{ base: "none", md: "flex" }}
+                >
+                  {Links.map((link) => (
+                    <NavLink key={link}>{link}</NavLink>
+                  ))}
+                </HStack>
               </HStack>
-            </HStack>
-          </Box>
-        </GridItem>
-      </Grid>
+            </Box>
+          </GridItem>
+        </Grid>
+      )}
     </Box>
   );
 }
---
+++ Components/NavbarMobile.jsx
@@ -0,0 +1,73 @@
+import { useRef, useState } from "react";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  IconButton,
+  Input,
+  Stack,
+  Button,
+  Box,
+  Icon,
+  InputGroup,
+  InputRightAddon,
+  Heading,
+} from "@chakra-ui/react";
+import { HamburgerIcon, ChevronDownIcon, SearchIcon } from "@chakra-ui/icons";
+import css from "../styles/Home.module.css";
+
+export default function NavbarMobile() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const firstField = useRef();
+
+  return (
+    <Box paddingLeft={"2%"} paddingTop={"3%"} id="SOY ESTEEE">
+      <IconButton
+        icon={<HamburgerIcon paddingLeft={"2%"} paddingTop={"3%"} />}
+        onClick={onOpen}
+        bg="black"
+        color="#9f231a"
+        w="10%"
+        fontSize="40px"
+      />
+
+      <Drawer
+        placement="right"
+        onClose={onClose}
+        isOpen={isOpen}
+        initialFocusRef={firstField}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <Box display="flex" alignItems="left">
+            <DrawerHeader fontWeight="900" color="whitesmoke" fontSize="30px">
+              My portfolio
+            </DrawerHeader>
+            <DrawerCloseButton border={"none"} bg="none" color="white" />
+          </Box>
+          <DrawerBody className={css.backgroundNavbarMobile}>
+            <Stack spacing={8}>
+              <a href="#AboutMe">
+                About me
+                <ChevronDownIcon />
+              </a>
+              <a href="#Projects">
+                Projects
+                <ChevronDownIcon />
+              </a>
+              <a href="#Contact">
+                Contact
+                <ChevronDownIcon />
+              </a>
+            </Stack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </Box>
+  );
+}
